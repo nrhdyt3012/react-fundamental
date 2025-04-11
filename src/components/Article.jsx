f34eccf2981737f9da4133998bf089ee60f97f6e
nrhdyt3012 <dayats354313@gmail.com>
2025-04-12 00:27:52 +0700

    conditional rendering2

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,13 @@
+
 function Article (props) {
     const tags = props.tags || [];
 
     return (
         <>
             <h3>{props.title}</h3>
-            <small>Date : {props.date}, tags: {tags.join(", ")}</small>
+            <small>Date : {props.date}, tags: {tags.join(", ")} 
+                {props.isNew ? "== Baru ==" : "== Lama =="}
+            </small>
         </>
     );
 }
