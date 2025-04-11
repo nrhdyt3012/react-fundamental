b3a1d88cf8e9c6f66974c7a76a46883f5c8c402f
nrhdyt3012 <dayats354313@gmail.com>
2025-04-11 17:24:33 +0700

    Fitur Filter Data dan Pengenalan useState

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import posts from "../posts.json";
 import Article from "../components/Article.jsx";
 
 function Homepage() {
+   
+    const [search, setSearch] = useState("");
+
+    const changeSearch =(event) => {
+        setSearch(event.target.value)
+    };
     return (
         <>
         <h1>Simple Blog</h1>
-        {posts.map((blog) => (
-<Article title={blog.title} tags={blog.tags} date={blog.date}></Article>
+        <div>Cari data: <input type="text" onChange={changeSearch}/></div>
+        <small>Ditemukan 0 hasil dengan kata {search}</small>
+        {posts.map(({title,tags, date}, index) => (
+<Article {...{title, tags, date}} key={index}></Article>
             ))}
         </>
     )
