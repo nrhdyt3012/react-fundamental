1e3ebdb88b5e86f2ecf5dda0b689e7356f236605
nrhdyt3012 <dayats354313@gmail.com>
2025-04-12 22:09:45 +0700

    Layouting dan Children Routes

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,15 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "../layouts/RootLayout";
 
 import Homepage from "../pages";
 import About from "../pages/About";
 
+
 export const router = createBrowserRouter ([
     {
         path:"/",
-        element: <Homepage></Homepage>
-    },
-    {
-        path:"/about",
-        element: <About></About>
+        element: <RootLayout></RootLayout>,
+        children: [
+            {
+                path:"/",
+                element :<Homepage></Homepage>
+            },
+            {
+                path:"/about",
+                element :<About></About>
+            }
+        ]
     }
 ]);
